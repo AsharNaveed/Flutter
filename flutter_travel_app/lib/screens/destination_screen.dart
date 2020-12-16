b50dec8ed505ff1eef2b244855fe8347852e585a
Ashar Naveed <58306646+AsharNaveed@users.noreply.github.com>
2020-12-16 23:18:50 +0500

    added files

---
+++ flutter_travel_app/lib/screens/destination_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_travel_app/models/destination_model.dart';
+
+class DestinationScreen extends StatefulWidget {
+  final Destination destination;
+
+  DestinationScreen({this.destination});
+  @override
+  DestinationScreen_State createState() => DestinationScreen_State();
+}
+
+// ignore: camel_case_types
+class DestinationScreen_State extends State<DestinationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
